3c5c094c2afe237a7a1338fe6d0465ff9853a9ab
TuanMinh926 <79306038+TuanMinh926@users.noreply.github.com>
2021-10-05 19:47:36 +0700

    Add files via upload

---
+++ OOP C++/Point1/Line.cpp
@@ -0,0 +1,35 @@
+#include "Point.cpp"
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class Line{
+private:
+    Point begin;
+    Point end;
+public:
+    Line(Point begin, Point end){
+        this->begin = begin;
+        this->end = end;
+    }
+    Line(int x1, int y1, int x2, int y2){
+        this->begin = Point(x1,y1);
+        this->end = Point(x2,y2);
+    }
+    void setBegin(Point begin){
+        this->begin = begin;
+    }
+    Point getBegin(){
+        return begin;
+    }
+    void setEnd(Point end){
+        this->end = end;
+    }
+    Point getEnd(){
+        return end;
+    }
+    double getLength(){
+        return sqrt(pow(end.getX()-begin.getX(),2)+pow(end.getY()-begin.getY(),2));
+    }
+};
